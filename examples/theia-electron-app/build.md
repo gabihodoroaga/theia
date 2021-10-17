b4f5c1f36809c92a8574eeb5c381c83025a7a3ea
Gabriel Hodoroaga <gabihodoroaga@gmail.com>
2021-10-17 23:59:08 +0300

    Add theia electron build

---
+++ examples/theia-electron-app/build.md
@@ -0,0 +1,10 @@
+# build theia electron app
+
+```bash 
+yarn
+
+yarn --cwd examples/theia-electron-app build
+yarn --cwd examples/theia-electron-app download:plugins
+yarn --cwd examples/theia-electron-app package 
+
+```
---
+++ examples/theia-electron-app/scripts/theia-electron-main.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const os = require('os')
+
+// Use a set of builtin plugins in our application.
+process.env.THEIA_DEFAULT_PLUGINS = `local-dir:${path.resolve(__dirname, '..', 'plugins')}`
+process.env.VSCODE_API_VERSION = "1.53.0"
+
+// Lookup inside the user's home folder for more plugins, and accept user-defined paths.
+process.env.THEIA_PLUGINS = [
+    process.env.THEIA_PLUGINS, `local-dir:${path.resolve(os.homedir(), '.theia', 'plugins')}`,
+].filter(Boolean).join(',')
+
+// Handover to the auto-generated electron application handler.
+require('../src-gen/frontend/electron-main.js')
